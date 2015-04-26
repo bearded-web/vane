9c43671ac19e3d61cc8137f4975230613b07b539
Anton Tiurin <noxiouz@yandex.ru>
2015-04-27 01:29:37 +0300

    [Test] Tests for checks.readme

--- vane/checks/readme_test.go
+++ vane/checks/readme_test.go
@@ -2,14 +2,19 @@ package checks
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/bearded-web/vane/vane/site"
 )
 
+//ToDO: add assert messages
 var readmefixtures = os.Getenv("FIXTURESPATH") + "/readme/"
 
 func TestReadMeBadStatus(t *testing.T) {
@@ -40,4 +45,44 @@ func TestReadMeCheckBody(t *testing.T) {
 	has, err := hasReadMeCheckResponse(testResp)
 	assert.NoError(t, err)
 	assert.True(t, has)
+}
+
+type ErroredReader struct{}
+
+func (e *ErroredReader) Read(p []byte) (n int, err error) {
+	return 0, fmt.Errorf("test error")
+}
+
+func TestReadMeBodyError(t *testing.T) {
+	testResp := &http.Response{
+		StatusCode: http.StatusOK,
+		Body:       ioutil.NopCloser(&ErroredReader{}),
+	}
+
+	has, err := hasReadMeCheckResponse(testResp)
+	assert.Error(t, err)
+	assert.False(t, has)
+}
+
+func TestReadMeURL(t *testing.T) {
+	s, _ := site.NewSite("http://testhost.net/")
+	assert.Equal(t, "http://testhost.net/"+readMePath, ReadMeURL(s))
+}
+
+func TestHasReadme(t *testing.T) {
+	tsReadme := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "not found", http.StatusNotFound)
+	}))
+	defer tsReadme.Close()
+
+	s, err := site.NewSite(tsReadme.URL)
+	assert.NoError(t, err)
+
+	has, err := HasReadMe(s)
+	assert.NoError(t, err)
+	assert.False(t, has)
+
+	s, _ = site.NewSite("http://127.0.0.1:9999")
+	has, err = HasReadMe(s)
+	assert.Error(t, err)
 }
